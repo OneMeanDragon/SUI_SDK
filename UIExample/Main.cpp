d29be40f0b86e1bc7c6f9694bc5e1f0fcc79861b
OneMeanDragon <dragonkings@gmail.com>
2023-06-11 21:40:23 -0400

    Initalize Warpfield
    .

---
+++ UIExample/Main.cpp
@@ -0,0 +1,244 @@
+#include "Framework.h"
+
+static std::wstring SdkBuildInformation{};
+
+#define BASE_FILE TEXT("File")
+#define BASE_TEST TEXT("Test")
+UINT MENU_TEST{ 0 };
+
+#pragma region "Extra Shit"
+const uiString AM_PM(int inBuf)
+{
+	if (inBuf >= 12) {
+		return uiString(TEXT("PM"));
+	}
+	return uiString(TEXT("AM"));
+}
+
+const uiString TimeStamp(void)
+{
+	uiChar timestamp[MAX_PATH]{ 0 };
+	SYSTEMTIME st;
+	GetLocalTime(&st);
+	const uiString szAMPM = AM_PM(st.wHour);
+	WORD fHour = st.wHour;
+	if (fHour > 12) { fHour -= 12; }
+	if (fHour == 0) { fHour += 12; }
+	//This string has a forced length, to change this edit the initalized value for *timestamp in Main.cpp
+#ifdef UNICODE
+	const wchar_t timeformat[]{ TEXT("[%02i:%02i:%02i %ls] ") };
+	swprintf_s(timestamp, timeformat, fHour, st.wMinute, st.wSecond, szAMPM.c_str());
+#else
+	const char timeformat[]{ TEXT("[%02i:%02i:%02i %s] ") };
+	sprintf_s(timestamp, timeformat, fHour, st.wMinute, st.wSecond, szAMPM);
+#endif
+	return uiString(timestamp);
+}
+#pragma endregion
+
+#pragma region "          UI QUEUE ARRAYS          "
+std::deque<sAddChat> AddTextArr{};
+std::deque<sAddTextA> AddTextArrA{};
+std::deque<sAddTextW> AddTextArrW{};
+#pragma endregion
+
+void DestroyVars() { /* these vars will mention a memory leak if not destroyed at destruct */
+	AddTextArr.~deque();
+	AddTextArrA.~deque();
+	AddTextArrW.~deque();
+	SdkBuildInformation.~basic_string();
+}
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nShowCmd) {
+#if _DEBUG
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+	//Load the application icon. (unless you want the default Icon)
+	//HICON Application_Icon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+
+	// Setup the UIInterface DLL
+	DWORD(WINAPI * InterfaceVersion)(VOID)           = 0;      // Predefined UiSdkVersion Function
+	DWORD(WINAPI * InterfaceFun)(LPVOID lpParameter) = 0;      // Predefined UIThread Function
+	std::wstring(WINAPI * BuildInf)(VOID)            = 0;      // Predefined UiSdkBuildInformation Function
+	HINSTANCE InterfaceModule = LoadLibrary(L"Interface.SUI"); // Load the UI DLL
+
+	if (InterfaceModule) {
+		*(void**)&InterfaceVersion = GetProcAddress(InterfaceModule, "UiSdkVersion");          //UiSdkVersion Address
+		*(void**)&InterfaceFun     = GetProcAddress(InterfaceModule, "UiThread");              //UIThread Address
+		*(void**)&BuildInf         = GetProcAddress(InterfaceModule, "UiSdkBuildInformation"); //UiSdkBuildInformation Address
+		uint32_t SdkVer{ 0 };
+		if (InterfaceVersion) {
+			SdkVer = InterfaceVersion();
+			SdkBuildInformation = BuildInf();
+		}
+		if (InterfaceFun && (SdkVer == UISDK_VERSION)) {
+			HANDLE hThread = CreateThread(NULL, 0, InterfaceFun, NULL /* NULL = default icon, (LPVOID)Application_Icon = our main thread icon */, 0, NULL);
+			HANDLE hEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+			MyMessageData data = { hEvent, GetCurrentThreadId() /* message data */ };
+			if (hThread != INVALID_HANDLE_VALUE && hThread != nullptr && hEvent != INVALID_HANDLE_VALUE && hEvent != nullptr) {
+				BOOL result = FALSE;
+				while (!result) { /* because our ui thread dosent become available immediatly */
+					Sleep(10);
+					result = PostThreadMessage(GetThreadId(hThread), WM_MY_MESSAGE, 0, reinterpret_cast<LPARAM>(&data));
+				}
+				WaitForSingleObject(hEvent, INFINITE);
+				CloseHandle(hEvent); /* our initalization is done */
+			}
+			else { /* Thread creation or Event creation failed */
+				ExitProcess(EXIT_FAILURE);
+			}
+
+			/*
+				UI is Loaded and ready to go
+			*/
+			MSG msg = { 0 };
+			while (TRUE) { /* main thread message pump */
+				if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+				{
+					/*
+					
+						This means you closed the UI Shutdown your APP
+
+					*/
+					if (msg.message == WM_QUIT || msg.message == WM_DESTROY)
+					{
+						OutputDebugString(TEXT("WM_QUIT | WM_DESTROY\r\n"));
+						//cleanup
+						DestroyVars();
+						break; /* this breaks the while loop our UI exited, only reason quit and destroy are out side the switch statement */
+					}
+					switch (msg.message) {
+						case WM_MY_MESSAGE: {
+							OutputDebugString(TEXT("event message\r\n"));
+							break;
+						}
+						case WM_UIINITIALIZED: { // send an add chat or some shit
+							/*
+								
+								This means Start your main Application shit
+
+							*/
+
+							/*
+								sAddChat is WIDE format only and it is here only if you want to customize
+								output colors
+							*/
+							//{
+							//	sAddChat test2;
+							//	test2.FmtMessage(TEXT("%s"), TimeStamp().c_str());
+							//	test2.FmtMessage(colors::darkslategray, TEXT("UIInit: %s\r\n"), TEXT("ack."));
+							//	test2.FmtMessage(TEXT("%s"), TimeStamp().c_str());
+							//	test2.FmtMessage(colors::teal, colors::deeppurple, TEXT("TestMessage: %s\r\n"), TEXT("just a message."));
+							//	/*
+							//		our struct must never go out of scope
+							//		and
+							//		our UI will tell us when to remove it
+							//		via: WM_UIPOPADDCHAT
+							//	*/
+							//	AddTextArr.push_back(test2);
+							//	PostThreadMessage(GetThreadId(hThread), WM_UIINITIALIZED_ACK, 0, reinterpret_cast<LPARAM>(&AddTextArr.front()));
+							//}
+							/*
+								sAddTextA sAddTextW
+								MB and UNICODE add text feature
+								sAddTextA: for those lazy fuckers that wont quit using multibyte mode.
+							*/
+							//{ // test sAddTextA
+							//	sAddTextA testA{};
+							//	testA.FmtMessage("%ls", TimeStamp().c_str());
+							//	testA.FmtMessage(colors::deeppurple, colors::yellow, "Testing AddText: %s\r\n", "A (MB)");
+							//	AddTextArrA.push_back(testA);
+							//	PostThreadMessage(GetThreadId(hThread), WM_UIADDTEXTA, 0, reinterpret_cast<LPARAM>(&AddTextArrA.front()));
+							//}
+							{ // test sAddTextW
+								sAddTextW testW{};
+								testW.FmtMessage(TEXT("%s"), TimeStamp().c_str());
+								testW.FmtMessage(colors::deeppurple, TEXT("%s\r\n"), SdkBuildInformation.c_str());
+								AddTextArrW.push_back(testW);
+								PostThreadMessage(GetThreadId(hThread), WM_UIADDTEXTW, 0, reinterpret_cast<LPARAM>(&AddTextArrW.front()));
+							}
+							// Add Menu Items
+							{
+								// Add Test to the Menu
+								PostThreadMessage(GetThreadId(hThread), WM_MENU_ADDCOMMANDW, 0, reinterpret_cast<LPARAM>(&BASE_TEST));
+							}
+							break;
+						}
+                        /////////////////////////////////////////////////////////
+						// Menu
+	  				    /////////////////////////////////////////////////////////
+						case WM_MENU_UPDATE: { // update our id holder
+							sMessageBoxText* menuname = reinterpret_cast<sMessageBoxText*>(msg.lParam);
+							if (menuname->MessageW == std::wstring(BASE_TEST)) {
+								MENU_TEST = msg.wParam;
+							}
+							break;
+						}
+						case WM_MENU_COMMAND: {
+							if (msg.wParam == MENU_TEST) {
+								sAddTextW testW{};
+								testW.FmtMessage(TEXT("%s"), TimeStamp().c_str());
+								testW.FmtMessage(colors::seagreen, TEXT("%s\r\n"), TEXT("Test menu command was pressed."));
+								AddTextArrW.push_back(testW);
+								PostThreadMessage(GetThreadId(hThread), WM_UIADDTEXTW, 0, reinterpret_cast<LPARAM>(&AddTextArrW.front()));
+							}
+							break;
+						}
+                        /////////////////////////////////////////////////////////
+
+						case WM_POPADDTEXTA: {
+							AddTextArrA.pop_front();
+							break;
+						}
+						case WM_POPADDTEXTW: {
+							AddTextArrW.pop_front();
+							break;
+						}
+						case WM_UIPOPADDCHAT: { // UI posted our message, remove it from the queue
+							if (AddTextArr.size() > 0) {
+								AddTextArr.pop_front();
+							}
+							break;
+						}
+						case WM_UIMESSAGEINPUT: { // Message from the Edit Input box
+							sMessageBoxText* pData = reinterpret_cast<sMessageBoxText*>(msg.lParam);
+							// once we are done processing this, queue that we are ready for more if any.
+
+							sAddTextW testW{};
+							testW.FmtMessage(TEXT("%s"), TimeStamp().c_str());
+							testW.FmtMessage(colors::gray, TEXT("%s\r\n"), pData->MessageW.c_str());
+							AddTextArrW.push_back(testW);
+							PostThreadMessage(GetThreadId(hThread), WM_UIADDTEXTW, 0, reinterpret_cast<LPARAM>(&AddTextArrW.front()));
+
+							// if this message is not sent back you will not recieve anymore edit process messages
+							PostThreadMessage(GetThreadId(hThread), WM_INPUTCOMPLETED, 0, NULL);
+							break;
+						}
+						default:
+						{
+							TranslateMessage(&msg);
+							DispatchMessage(&msg);
+						}
+					}
+				} else {
+					Sleep(10);
+				}
+			}
+
+			// Wait for the ui thread to exit, if it hasnt yet done so.
+			WaitForSingleObject(hThread, INFINITE);
+			CloseHandle(hThread);
+		}
+		/*
+			2 reasons to get here Thread process finally completed
+			or we failed to retrieve the UIThread Function
+		*/
+		FreeLibrary(InterfaceModule);
+	} else { // failed to load the interface
+		ExitProcess(EXIT_FAILURE);
+	}
+	/*
+		last chance to cleanly kill your application
+	*/
+	ExitProcess(EXIT_SUCCESS);
+}
---
+++ UIExample/UI_SDK.h
@@ -0,0 +1,354 @@
+#if !defined(UIDLLSDK_HEADER)
+#define UIDLLSDK_HEADER
+#pragma once
+
+/*
+
+	This SDK is for use with the Interface.SUI
+	
+	if there have been any updates to the interface
+	check that the WM_ messages are still relevent
+
+*/
+#define UISDK_VERSION 3
+
+#pragma region "Init message"
+struct MyMessageData // our initialize message
+{
+	HANDLE hEvent;
+	DWORD fromThreadID; // id of the main thread so we can send messages back
+};
+#pragma endregion
+
+#define WM_MY_MESSAGE        (WM_USER + 1001) //main thread giving its info for initialization
+#define WM_MYUI_MESSAGE      (WM_USER + 1002) //ui to gui
+#define WM_UIINITIALIZED     (WM_USER + 1003) //ui telling the main thread its up and running
+#define WM_UIINITIALIZED_ACK (WM_USER + 1004) //main thread acknoledging the init
+
+#define WM_UIADDCHAT         (WM_USER + 1005) //ui thread to the gui to add chat
+#define WM_UIPOPADDCHAT      (WM_USER + 1006) //ui telling the main thread to pop the message
+#define WM_MTREQADDCHAT      (WM_USER + 1007) //Main thread request add chat
+
+#define WM_UIMESSAGEINPUT    (WM_USER + 1008) // Message input from the editbox
+#define WM_INPUTCOMPLETED    (WM_USER + 1009) // Main Thread tells UI its ready for more input (if any exists).
+
+#define WM_UIADDTEXTA        (WM_USER + 1010) // sAddTextA
+#define WM_UIADDTEXTW        (WM_USER + 1011) // sAddTextW
+#define WM_POPADDTEXTA       (WM_USER + 1012) // UI completed its sAddTextA
+#define WM_POPADDTEXTW       (WM_USER + 1013) // UI completed its sAddTextW
+
+#define WM_MENU_UPDATE              (WM_USER + 1014) // UI->Thread (UI telling point of origin menu command data [Name + id])
+#define WM_MENU_COMMAND             (WM_USER + 1015)
+#define WM_MENU_ADDCOMMANDA         (WM_USER + 1016)
+#define WM_MENU_ADDCOMMANDW         (WM_USER + 1017)
+#define WM_MENU_ADDSUBMENUA         (WM_USER + 1018)
+#define WM_MENU_ADDSUBMENUW         (WM_USER + 1019)
+#define WM_MENU_ADDSUBMENUCOMMANDA  (WM_USER + 1020)
+#define WM_MENU_ADDSUBMENUCOMMANDW  (WM_USER + 1021)
+
+#pragma region "COLORS"
+enum class colors : uint32_t {
+	black,
+	green,
+	silver,
+	lime,
+	seagreen,
+	gray,
+	darkslategray,
+	olive,
+	white,
+	yellow,
+	maroon,
+	navy,
+	red,
+	blue,
+	purple,
+	teal,
+	fuchsia,
+	aqua,
+	deeppurple
+};
+inline COLORREF ColorByName(const colors name)
+{
+	switch (name) {
+	case colors::black: { return 0x000000L; } //Black
+	case colors::green: { return 0x008000L; } //Green
+	case colors::silver: { return 0xc0c0c0L; } //Silver 0xc0c0c0L
+	case colors::lime: { return 0x00ff00L; } //Lime 
+	case colors::seagreen: { return 0x32CD32L; } //seagreen 0x80CD43L 65B0161C
+	case colors::gray: { return 0x808080L; } //Gray //old 0x808080L
+	case colors::darkslategray: { return 0x2F4F4FL; } //DarkSlateGray
+	case colors::olive: { return 0x008080L; } //Olive
+	case colors::white: { return 0xffffffL; } //White
+	case colors::yellow: { return 0x00CDCDL; } //Yellow //old 0x00ffffL //gold3 0x00ADCDL
+	case colors::maroon: { return 0x000080L; } //Maroon
+	case colors::navy: { return 0x800000L; } //Navy
+	case colors::red: { return 0x0000eeL; } //Red
+	case colors::blue: { return 0xCD5F3AL; } //Blue //0xCD5F3AL //old 0xff0000L (to much blur on a black background)
+	case colors::purple: { return 0x800080L; } //Purple
+	case colors::teal: { return 0x808000L; } //Teal
+	case colors::fuchsia: { return 0xff00ffL; } //Fuchsia 
+	case colors::aqua: { return 0xffff00L; } //Aqua 
+	case colors::deeppurple: { return 0xDB7093L; } //deeppurple 
+	default: {return (COLORREF)0x000000L; }
+	} //orange = 0x3A5FCDL
+}
+#pragma endregion
+
+#pragma region "ADD CHAT STRUCT'S (CURRENTLY ONLY WIDE)"
+// if more is needed the UI will need an update
+#define MAX_ADDTEXTBUFFER 1024
+
+/* just a prototype for ui message ping pongs */
+/*
+	This is WIDESTR only and is only here if your 
+	wanting to test colors or make additions to 
+	the color listing, do note that if you add a
+	color put them at the bottom of the enum class
+
+*/
+struct sAddChat {
+	uint32_t messagecounter{ 0 };
+	std::vector<COLORREF> forColorList{};
+	std::vector<COLORREF> bakColorList{};
+	std::vector<uiString> MessageList{};
+
+#ifdef TEST_ARRAY_DESTRUCT
+	~test() {
+		FreeMessages();
+		forColorList.~vector();
+		bakColorList.~vector();
+		MessageList.~vector();
+	}
+#endif
+
+	/// <summary>
+	/// Default white text on black background
+	/// </summary>
+	/// <param name="format"></param>
+	/// <param name=""></param>
+	void FmtMessage(const uiChar* format, ...) {
+		std::vector<uiChar> buffer{};
+		buffer.resize(256);
+		va_list args;
+		va_start(args, format);
+#ifdef UNICODE
+		/* vswprintf was changed to conform with the ISO C standard (buffer size) */
+		vswprintf(buffer.data(), buffer.size(), format, args);
+#else
+		/* mb version of the above */
+		vsprintf_s(buffer.data(), buffer.size(), format, args);
+#endif
+		va_end(args);
+
+		/* add color to list */
+		forColorList.emplace_back(ColorByName(colors::white));
+		bakColorList.emplace_back(ColorByName(colors::black));
+		/* add message to the list */
+		MessageList.emplace_back(uiString(buffer.data()));
+		messagecounter++;
+	}
+
+	/// <summary>
+	/// Default black background
+	/// </summary>
+	/// <param name="forcolor"></param>
+	/// <param name="format"></param>
+	/// <param name=""></param>
+	void FmtMessage(const colors forcolor, const uiChar* format, ...) {
+		std::vector<uiChar> buffer{};
+		buffer.resize(256);
+		va_list args;
+		va_start(args, format);
+#ifdef UNICODE
+		/* vswprintf was changed to conform with the ISO C standard (buffer size) */
+		vswprintf(buffer.data(), buffer.size(), format, args);
+#else
+		/* mb version of the above */
+		vsprintf_s(buffer.data(), buffer.size(), format, args);
+#endif
+		va_end(args);
+
+		/* add color to list */
+		forColorList.emplace_back(ColorByName(forcolor));
+		bakColorList.emplace_back(ColorByName(colors::black));
+		/* add message to the list */
+		MessageList.emplace_back(uiString(buffer.data()));
+		messagecounter++;
+	}
+
+	/// <summary>
+	/// no default values they must be set
+	/// </summary>
+	/// <param name="forcolor"></param>
+	/// <param name="backcolor"></param>
+	/// <param name="format"></param>
+	/// <param name=""></param>
+	void FmtMessage(const colors forcolor, const colors backcolor, const uiChar* format, ...)
+	{
+		std::vector<uiChar> buffer{};
+		buffer.resize(256);
+		va_list args;
+		va_start(args, format);
+#ifdef UNICODE
+		/* vswprintf was changed to conform with the ISO C standard (buffer size) */
+		vswprintf(buffer.data(), buffer.size(), format, args);
+#else
+		/* mb version of the above */
+		vsprintf_s(buffer.data(), buffer.size(), format, args);
+#endif
+		va_end(args);
+
+		/* add color to list */
+		forColorList.emplace_back(ColorByName(forcolor));
+		bakColorList.emplace_back(ColorByName(backcolor));
+		/* add message to the list */
+		MessageList.emplace_back(uiString(buffer.data()));
+		messagecounter++;
+	}
+
+	/// <summary>
+	/// Clear the message arrays
+	/// </summary>
+	void FreeMessages() {
+		while (MessageList.size() > 0 && forColorList.size() > 0 && bakColorList.size() > 0)
+		{
+			forColorList.pop_back();
+			bakColorList.pop_back();
+			MessageList.pop_back();
+			messagecounter--;
+		}
+		forColorList.clear();
+		bakColorList.clear();
+		MessageList.clear();
+	}
+};
+
+struct sAddTextA {
+	uint32_t messagecounter{ 0 };
+	std::vector<colors> forColorList{};
+	std::vector<colors> bakColorList{};
+	std::vector<std::string> MessageList{};
+
+	// w on b default
+	void FmtMessage(const char* format, ...) {
+		std::vector<char> buffer{};
+		buffer.resize(MAX_ADDTEXTBUFFER);
+		va_list args;
+		va_start(args, format);
+		vsprintf_s(buffer.data(), (MAX_ADDTEXTBUFFER - 1), format, args);
+		va_end(args);
+
+		/* add color to list */
+		forColorList.emplace_back(colors::white);
+		bakColorList.emplace_back(colors::black);
+		/* add message to the list */
+		MessageList.emplace_back(std::string(buffer.data()));
+		messagecounter++;
+	}
+
+	// for on b default
+	void FmtMessage(const colors forcolor, const char* format, ...) {
+		std::vector<char> buffer{};
+		buffer.resize(MAX_ADDTEXTBUFFER);
+		va_list args;
+		va_start(args, format);
+		vsprintf_s(buffer.data(), (MAX_ADDTEXTBUFFER - 1), format, args);
+		va_end(args);
+
+		/* add color to list */
+		forColorList.emplace_back(forcolor);
+		bakColorList.emplace_back(colors::black);
+		/* add message to the list */
+		MessageList.emplace_back(std::string(buffer.data()));
+		messagecounter++;
+	}
+
+	void FmtMessage(const colors forcolor, const colors backcolor, const char* format, ...)
+	{
+		std::vector<char> buffer{};
+		buffer.resize(MAX_ADDTEXTBUFFER);
+		va_list args;
+		va_start(args, format);
+		vsprintf_s(buffer.data(), (MAX_ADDTEXTBUFFER - 1), format, args);
+		va_end(args);
+
+		/* add color to list */
+		forColorList.emplace_back(forcolor);
+		bakColorList.emplace_back(backcolor);
+		/* add message to the list */
+		MessageList.emplace_back(std::string(buffer.data()));
+		messagecounter++;
+	}
+
+};
+
+struct sAddTextW {
+	uint32_t messagecounter{ 0 };
+	std::vector<colors> forColorList{};
+	std::vector<colors> bakColorList{};
+	std::vector<std::wstring> MessageList{};
+
+	// w on b default
+	void FmtMessage(const wchar_t* format, ...) {
+		std::vector<wchar_t> buffer{};
+		buffer.resize(MAX_ADDTEXTBUFFER);
+		va_list args;
+		va_start(args, format);
+		vswprintf(buffer.data(), (MAX_ADDTEXTBUFFER - 1), format, args);
+		va_end(args);
+
+		/* add color to list */
+		forColorList.emplace_back(colors::white);
+		bakColorList.emplace_back(colors::black);
+		/* add message to the list */
+		MessageList.emplace_back(std::wstring(buffer.data()));
+		messagecounter++;
+	}
+
+	// for on b default
+	void FmtMessage(const colors forcolor, const wchar_t* format, ...) {
+		std::vector<wchar_t> buffer{};
+		buffer.resize(MAX_ADDTEXTBUFFER);
+		va_list args;
+		va_start(args, format);
+		vswprintf(buffer.data(), (MAX_ADDTEXTBUFFER - 1), format, args);
+		va_end(args);
+
+		/* add color to list */
+		forColorList.emplace_back(forcolor);
+		bakColorList.emplace_back(colors::black);
+		/* add message to the list */
+		MessageList.emplace_back(std::wstring(buffer.data()));
+		messagecounter++;
+	}
+
+	void FmtMessage(const colors forcolor, const colors backcolor, const wchar_t* format, ...)
+	{
+		std::vector<wchar_t> buffer{};
+		buffer.resize(MAX_ADDTEXTBUFFER);
+		va_list args;
+		va_start(args, format);
+		vswprintf(buffer.data(), (MAX_ADDTEXTBUFFER - 1), format, args);
+		va_end(args);
+
+		/* add color to list */
+		forColorList.emplace_back(forcolor);
+		bakColorList.emplace_back(backcolor);
+		/* add message to the list */
+		MessageList.emplace_back(std::wstring(buffer.data()));
+		messagecounter++;
+	}
+
+};
+
+#pragma endregion
+
+#pragma region "EDITBOX PROCESS MESSAGES"
+struct sMessageBoxText {
+	std::string  MessageA{};
+	std::wstring MessageW{};
+};
+#pragma endregion
+
+#endif
